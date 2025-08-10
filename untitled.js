4e52f6c48dfa267b8e518e1f36722f7c2ee9b37b
Md Shamim Ahmed <mdshamimhosen682@gmail.com>
2025-08-10 17:37:57 +0600

    Delete untitled.js
    Signed-off-by: Md Shamim Ahmed <mdshamimhosen682@gmail.com>

--- untitled.js
+++
@@ -1,8 +0,0 @@
-let tg = window.Telegram.WebApp;
-
-document.getElementById("helloBtn").addEventListener("click", () => {
-  tg.showAlert("Hello from Mini App!");
-});
-
-// Optional: expand app to full height
-tg.expand();
